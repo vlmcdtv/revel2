337d5630f1adeacf81304126ab6983341ee9e750
vlmcdtv <vivian.moser12@gmail.com>
2018-11-29 20:40:41 -0800

    final for now

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import '@/assets/main.css' 
+import "@/assets/main.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
